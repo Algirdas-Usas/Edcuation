3d9830d2644360f43c35236416026bc7bd6f7d2c
Algirdas-Usas <algirdas.usakauskas@gmail.com>
2025-11-15 18:00:27 +0100

    LogikTest.java
    ändrad

--- Main/src/WordCounter/Test/LogikTest.java
+++ Main/src/WordCounter/Test/LogikTest.java
@@ -6,26 +6,29 @@ public class LogikTest {
     @Test
     void testEnLangRad() {
         Logik logik = new Logik();
-        logik.raknaRad("Hej på dig, fint väder idag");
+        logik.raknaRad("Hej på dig, fint väder idag"); // 22 tecken (mellanslag ignoreras)
         assertEquals(1, logik.getAntalRader(), "Antal rader ska vara 1");
-        assertEquals(3, logik.getAntalTecken(), "Antal tecken ska vara 27");
+        assertEquals(22, logik.getAntalTecken(), "Antal tecken ska vara 22");
     }
 
     @Test
-    void testFleraRader() {
+    void testutanmellanrum() {
         Logik logik = new Logik();
-        logik.raknaRad("Hej");
-        logik.raknaRad("på");
-        logik.raknaRad("dig");
-        assertEquals(3, logik.getAntalRader(), "Antal rader ska vara 3");
-        assertEquals(3 + 2 + 3, logik.getAntalTecken(), "Antal tecken ska vara summan av alla rader");
+        logik.raknaRad("Hej  ");          // 3 tecken (mellanslag ignoreras)
+        logik.raknaRad("på dig!");        // 6 tecken (”på” 2 + ”dig!” 4)
+        logik.raknaRad("  :)  ");        // 2 tecken (":)" är två, mellanslag ignoreras)
+        logik.raknaRad("");               // 0 tecken
+
+        assertEquals(4, logik.getAntalRader(), "Ska vara 4 rader totalt");
+        assertEquals(3 + 6 + 2 + 0, logik.getAntalTecken(), "Tecken räknas utan mellanslag");
     }
 
     @Test
     void testTomRad() {
         Logik logik = new Logik();
-        logik.raknaRad("");
+        logik.raknaRad(""); //0 tecken
         assertEquals(1, logik.getAntalRader(), "Tom rad räknas som en rad");
         assertEquals(0, logik.getAntalTecken(), "Tom rad ska inte lägga till tecken");
     }
 }
+
